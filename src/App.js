d5b8a17243420625317f499aa515b02209bac57e
ivanovda010101 <ivanovda010101@gmail.com>
2023-11-09 00:54:16 +0300

    MASTER: added query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    const handleTextChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    function handleSendButton() {
+        const queryParam = JSON.stringify({message})
+        console.log(queryParam)
+        fetch('http://localhost:8080/article', {
+            mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({text}),
+            body: queryParam
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
+
+    return (
+        <div>
+            <textarea value={message} onChange={handleTextChange} />
+            <button onClick={handleSendButton}>
+            </button>
+        </div>
+    );
 }
 
 export default App;
